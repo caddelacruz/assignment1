b54893f82b0d520166af704e84f45a20f3eeb22c
caddelacruz <152310179+caddelacruz@users.noreply.github.com>
2024-10-15 10:22:13 -0600

    Update Cone.java

--- src/shape/Cone.java
+++ src/shape/Cone.java
@@ -1,10 +1,1 @@
-package shape;
 
-public class Cone implements Comparable <Cone> 
-{
-    String cylinder;
-    String octagonalPrism;
-    String pentagonalPrism;
-    String pyramid;
-    String squarePrism;
-}
